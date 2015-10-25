8819d0a35e4852dba141cb316abc7423a5ef2886
Sergio Castillo <s.cast.lara@gmail.com>
2015-10-25 19:23:49 +0100

    Nothing very important.

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 # Set the install and executable directory to whatever you want
-
 # BEWARE: DEPENDING ON YOUR CHOICE YOU MAY NEED ROOT PRIVILEGES!
 
-echo "Where do you want to install dotcompare? Enter absolute path:"
+echo "Where do you want to install dotcompare? Enter absolute path:";
 read INSTALLDIR;
-EXECDIR="/usr/local/bin"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+EXECDIR="/usr/local/bin";
 mkdir $INSTALLDIR/dotcompare;
-cp -r ./* $INSTALLDIR/dotcompare;
+cp -r $DIR/* $INSTALLDIR/dotcompare;
 ln -s $INSTALLDIR/dotcompare/dotcompare.pl /usr/local/bin/dotcompare;
 echo "dotcompare INSTALLED on $INSTALLDIR";
-
