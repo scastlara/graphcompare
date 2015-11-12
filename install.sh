b136c725eb3414973c0a635f5e012226bad7b72e
Sergio Castillo <s.cast.lara@gmail.com>
2015-11-12 15:30:33 +0100

    Now checks if directories and symlinks exist.

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@
 # script. If you don't have them, you may need to copy the files
 # wherever you want manually.
 
-echo "Where do you want to install dotcompare? Enter absolute path:";
+echo "Where do you want to install dotcompare?";
+echo "Enter ABSOLUTE path:";
 read INSTALLDIR;
 
 # Directory where the program and all its files are.
@@ -14,12 +15,42 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 EXECDIR="/usr/local/bin";
 
 # INSTALLING PROGRAM
-mkdir $INSTALLDIR/dotcompare;
+
+echo;
+echo "# INSTALLING dotcompare in $INSTALLDIR";
+
+# Does the directory already exist?
+[ -d $INSTALLDIR/dotcompare ] \
+&& ( echo && echo "    $INSTALLDIR/dotcompare already exists! Updating program..." ) \
+|| mkdir $INSTALLDIR/dotcompare;
+
+# Update or create files
 cp -r $DIR/* $INSTALLDIR/dotcompare;
+chmod a+x $INSTALLDIR/dotcompare/dotcompare.pl;
+
+echo;
+echo "# CREATING SYMLINK";
+# Does the symlink already exist?
+[ -L /usr/local/bin/dotcompare ] \
+&& ( echo && echo "    Updating symlink to (possibly) new location" && rm /usr/local/bin/dotcompare ) \
+|| echo "    Creating symlink in /usr/local/bin";
+# Create symlink
 ln -s $INSTALLDIR/dotcompare/dotcompare.pl /usr/local/bin/dotcompare;
-echo "dotcompare INSTALLED on $INSTALLDIR";
+
+echo;
+echo "# dotcompare INSTALLED on $INSTALLDIR";
+echo;
+
+# TESTING
+echo "# TESTING dotcompare";
+chmod a+x $INSTALLDIR/dotcompare/tests/test.sh;
+$INSTALLDIR/dotcompare/tests/test.sh;
 
 # CREATING AND SAVING MANPAGE
 MANDIR="/usr/share/man/man1";
-pod2man $DIR/dotcompare.pl | gzip > $MANDIR/dotcompare.1.gz
+pod2man $DIR/dotcompare.pl | gzip > $MANDIR/dotcompare.1.gz;
+
+echo;
 echo "man page added to $MANDIR";
+echo "use \"man dotcompare\" to see the manual page";
+echo;
