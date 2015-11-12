d8b27ab8d31b0c871e1979e7e5624ac0346483da
Sergio Castillo <s.cast.lara@gmail.com>
2015-11-12 15:31:10 +0100

    Now checks if svg output is correct.

--- tests/test.sh
+++ tests/test.sh
@@ -3,43 +3,86 @@
 # Directory of the test script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
-# Create new tables
-    # 2table
-perl $DIR/../dotcompare.pl -f $DIR/iHOP.dot,$DIR/Sparser.dot -t $DIR/2table.tmp >/dev/null 2> /dev/null;
-    # 3table
-perl $DIR/../dotcompare.pl -f $DIR/AllGraphs.dot,$DIR/iHOP.dot,$DIR/Sparser.dot -t $DIR/3table.tmp >/dev/null 2> /dev/null;
-
-
 # TESTING
-echo "";
-echo "Testing dotcompare counts...";
 echo "";
 ERROR=0;
 
+# COUNTS
+echo "Testing dotcompare counts...";
+echo "";
+
+# CREATE NEW TABLES
+# 2table
+perl $DIR/../dotcompare.pl \
+-f $DIR/iHOP.dot,$DIR/Sparser.dot \
+-t $DIR/2table.tmp >/dev/null 2> /dev/null;
+# 3table
+perl $DIR/../dotcompare.pl \
+-f $DIR/AllGraphs.dot,$DIR/iHOP.dot,$DIR/Sparser.dot \
+-t $DIR/3table.tmp >/dev/null 2> /dev/null;
+
+
 # TWO DOT FILES
 if diff -q $DIR/2table.tmp $DIR/2table > /dev/null ; then
-    echo "Two DOT files... ok";
+    echo "count 2 DOT files... ok";
 else
     ((ERROR++));
-    echo "Two DOT files... not ok"
+    echo "count 2 DOT files... not ok"
 fi;
 
 # THREE DOT FILES
 if diff -q $DIR/3table.tmp $DIR/3table > /dev/null ; then
-    echo "Three DOT files... ok";
+    echo "count 3 DOT files... ok";
 else
     ((ERROR++));
-    echo "Three DOT files... not ok"
+    echo "count 3 DOT files... not ok"
 fi;
-echo "";
+echo;
 
-# Remove temp files
+
+# SVGs
+echo;
+echo "Testing dotcompare venn svg output...";
+echo;
+
+# CREATE NEW SVGs
+# 2svg
+perl $DIR/../dotcompare.pl \
+-f $DIR/Sparser.dot,$DIR/iHOP.dot \
+-v $DIR/2svg.tmp >/dev/null 2> /dev/null;
+# 3svg
+perl $DIR/../dotcompare.pl \
+-f $DIR/AllGraphs.dot,$DIR/iHOP.dot,$DIR/Sparser.dot \
+-v $DIR/3svg.tmp >/dev/null 2> /dev/null;
+
+# TWO DOT FILES
+if diff -q $DIR/2svg.tmp $DIR/2svg > /dev/null ; then
+    echo "svg 2 DOT files... ok";
+else
+    ((ERROR++));
+    echo "svg 2 DOT files... not ok"
+fi;
+
+# THREE DOT FILES
+if diff -q $DIR/3svg.tmp $DIR/3svg > /dev/null ; then
+    echo "svg 3 DOT files... ok";
+else
+    ((ERROR++));
+    echo "svg 3 DOT files... not ok"
+fi;
+
+
+
+# Remove all temp files
 rm $DIR/2table.tmp;
 rm $DIR/3table.tmp;
+rm $DIR/2svg.tmp;
+rm $DIR/3svg.tmp;
 
+echo;
 if [ $ERROR -ne 0 ]; then
     echo "Something is wrong: $ERROR number of errors";
 else 
     echo "Everything is fine: $ERROR number of errors";
 fi;
-echo "";
+echo;
